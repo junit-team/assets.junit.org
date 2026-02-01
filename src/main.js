07779c42a80d2909b44892eb78745c8cc768c961
Marc Philipp <mail@marcphilipp.de>
2026-02-01 21:16:46 +0100

    Include all Blinker font weights

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import "@fontsource-variable/inter/wght.css";
 import "@fontsource-variable/inter/wght-italic.css";
 import "@fontsource-variable/jetbrains-mono/wght.css";
 import "@fontsource-variable/jetbrains-mono/wght-italic.css";
-import "@fontsource/blinker/600.css";
+import "@fontsource/blinker/latin.css";
+import "@fontsource/blinker/latin-ext.css";
